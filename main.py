1ecbb5630cf65d0d27932ccabf036206f9a750f6
nnaeyno <ngogo20@freeuni.edu.ge>
2024-09-12 01:58:54 +0400

    timing info

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from data_processing import JSONDataWriter, MultiThreadDataProcessor, URLDataFetcher
 
 
@@ -9,6 +11,7 @@ def construct_urls(base_url: str):
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     base_url = "https://jsonplaceholder.typicode.com/posts/"
     output_file = "data.json"
 
@@ -21,6 +24,6 @@ if __name__ == "__main__":
 
     try:
         processor.process(urls, output_file)
-        print(f"Data successfully fetched from {base_url} and saved to {output_file}.")
+        print(f"Data successfully fetched from {base_url} and saved to {output_file}. {time.time() - start_time} seconds")
     except Exception as e:
         print(f"An error occurred: {e}")
